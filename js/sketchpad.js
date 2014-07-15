712545f068417f718de47384bee169343e372b2c
Benjamin Ratiarisolo <benjamin.ratiarisolo@fr.ibm.com>
2014-07-16 01:16:59 +0200

    Added button and  grid regeneration

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,4 +1,3 @@
-var grid_size = 64;
 var minSize = 10;
 var backgroundColor = "green";
 var foregroundColor = "blue";
@@ -11,39 +10,49 @@ var randomRGB = function() {
 	return "rgb(" + random255() +"," + random255() +"," + random255() +")";
 }
 
-$(document).ready(function() {
-	var $body = $(this.ie6 ? document.body : document); // using document in ie6 causes a crash
-	var width = $body.width();
-	var height = $body.height();
+var resetBoxShadow = function() {
+	this.style.boxShadow = "inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2), 0 8px 6px -6px black";
+}
 
-	var rowHeight = Math.floor((height - grid_size - 1)/ grid_size);
-	var cellWidth = Math.floor((width - grid_size - 1) / grid_size);
-	if (rowHeight < cellWidth) {
-		rowHeight = cellWidth;
-	} else {
-		cellWidth = rowHeight;
-	}
-	if (rowHeight < minSize) {
+var generateGrids = function(width, height, nbGrids) {
+	if ((nbGrids * (minSize + 1) + 1) > width) {
+		nbGrids = Math.floor(width / (minSize + 1) - 1);		
 		rowHeight = minSize;
 		cellWidth = minSize;
-		//$body.width(minSize * grid_size + grid_size + 1);
+		console
+	} else {
+		var rowHeight = Math.floor((width - nbGrids - 1)/ nbGrids);
+		var cellWidth = Math.floor((height - nbGrids - 1) / nbGrids);
+		if (rowHeight < cellWidth) {
+			rowHeight = cellWidth;
+		} else {
+			cellWidth = rowHeight;
+		}
+		if (rowHeight < minSize) {
+			rowHeight = minSize;
+			cellWidth = minSize;
+			//$body.width(minSize * nbGrids + nbGrids + 1);
+		}
 	}
 	console.log("Width: " + width + " |Height: " + height);
 	console.log("rowHeight: " + rowHeight + " |cellWidth: " + cellWidth);
+	console.log("nbGrids: " + nbGrids);
 	var grids = [];
-	for (var gridRowIndex = 0; gridRowIndex < grid_size; ++gridRowIndex) {
+	for (var gridRowIndex = 0; gridRowIndex < nbGrids; ++gridRowIndex) {
 		grids.push('<div class="grid-row">');
-		for (var gridColumnIndex = 0; gridColumnIndex < grid_size; ++gridColumnIndex) {
-			//grids.push('<div class="grid">' + (gridRowIndex * grid_size + gridColumnIndex + 1) + '</div>');
+		for (var gridColumnIndex = 0; gridColumnIndex < nbGrids; ++gridColumnIndex) {
+			//grids.push('<div class="grid">' + (gridRowIndex * nbGrids + gridColumnIndex + 1) + '</div>');
 			grids.push('<div class="grid"></div>');
 		}
 		grids.push('</div>');
 	}	
-	var $section = $('#grid-container');
-	$section.append(grids.join(''));
+	var section = document.getElementById('grid-container');
+	section.innerHTML = grids.join('');
 
+
+    //var css = '.grid{width:' + cellWidth + 'px;}.grid-row{height:' + rowHeight +'px;}.grid-container{height:' + gridSize +'px;width:' + gridSize +'px;}';
     var css = '.grid{width:' + cellWidth + 'px;}.grid-row{height:' + rowHeight +'px;}';
-    console.log('style: ' +css);
+    //console.log('style: ' +css);
 	var $style = $('<style type="text/css"></style>').appendTo('head'); 
     $style.html(css);
     $('.grid').mouseenter(function() {
@@ -59,4 +68,20 @@ $(document).ready(function() {
 	    	this.hasChangedColor = true;
     	}
 	});
+}
+
+$(document).ready(function() {
+	var $body = $(this.ie6 ? document.body : document); // using document in ie6 causes a crash
+	var width = $body.width();
+	var height = $body.height();
+
+	var button = document.getElementById('button');
+	button.onmousedown = function() {
+		this.style.boxShadow = "inset 0 1px 4px rgba(0, 0, 0, 0.6)";
+		var nbGrids = prompt("How many grids per side shall be generated?");
+		generateGrids(width, height, nbGrids);
+	};	
+	button.onmouseup = resetBoxShadow;
+	button.onmouseout = resetBoxShadow;
+	generateGrids(width, height, 150);
 });
